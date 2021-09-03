676ca7b477e0413317603be5d5b0f5be61d0f629
SirajYusuf <88265773+SirajYusuf@users.noreply.github.com>
2021-09-03 15:02:14 +0530

    Added humidity

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -1,14 +1,6 @@
 const request = require('request')
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+
 
 
 
@@ -20,7 +12,7 @@ const forecast = (latitude, longitude ,callback) => {
     }else if(body.error){
         callback('Unable to find location')
     }else{ 
-       callback(undefined, body.current.weather_descriptions+". It is currently " + body.current.temperature +" degrees out. It feels like " + body.current.feelslike + " degrees out.")
+       callback(undefined, body.current.weather_descriptions+".It is currently " + body.current.temperature +" degrees out. It feels like " + body.current.feelslike + " degrees out.The humidity is " + body.current.humidity + "%")
     }
    })
 
